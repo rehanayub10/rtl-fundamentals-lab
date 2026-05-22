326e7cd3ad3432f7127ea14158c9afe958dc1888
rehanayub10 <rehanayub20@gmail.com>
2026-05-21 23:40:06 -0400

    adding rtl for paramterized mux

--- blocks/mux/rtl/mux.sv
+++ blocks/mux/rtl/mux.sv
@@ -1,12 +1,25 @@
 
-module mux (
-    input  logic a,    // Input 0
-    input  logic b,    // Input 1
-    input  logic sel,   // Select line
-    output logic out    // Output
+//module mux (
+//    input  logic a,    // Input 0
+//    input  logic b,    // Input 1
+//    input  logic sel,   // Select line
+//    output logic out    // Output
+//);
+//
+//    // If sel is 0, out = a; if sel is 1, out = b
+//    assign out = sel ? b : a;
+//
+//endmodule
+
+module mux #(
+    parameter int WIDTH  = 8,
+    parameter int INPUTS = 4
+) (
+    input  logic [WIDTH-1:0]          in  [INPUTS],
+    output logic [WIDTH-1:0]          out,
+    input  logic [$clog2(INPUTS)-1:0] sel
 );
 
-    // If sel is 0, out = a; if sel is 1, out = b
-    assign out = sel ? b : a;
+    assign out = in[sel];
 
 endmodule
